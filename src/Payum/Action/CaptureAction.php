92f1e1bb943f94c2da82b4f988e150a286c4faef
Laurentiu Cocos <laurentiu.cocos@crystalcode.eu>
2020-03-01 17:18:30 +0200

    [GENERAL] payment capture implemented

--- src/Payum/Action/CaptureAction.php
+++ src/Payum/Action/CaptureAction.php
@@ -13,48 +13,46 @@ declare(strict_types=1);
 namespace Infifni\SyliusEuPlatescPlugin\Payum\Action;
 
 use Infifni\SyliusEuPlatescPlugin\Payum\SyliusApi;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
 use Infifni\SyliusEuPlatescPlugin\Utils\EuPlatescHelper;
 use Payum\Core\Action\ActionInterface;
 use Payum\Core\ApiAwareInterface;
 use Payum\Core\Exception\RequestNotSupportedException;
 use Payum\Core\Exception\UnsupportedApiException;
-use Sylius\Component\Addressing\Model\CountryInterface;
+use Payum\Core\Reply\HttpPostRedirect;
 use Sylius\Component\Core\Model\AddressInterface;
 use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
-use Sylius\Component\Core\Model\PaymentInterface as SyliusPaymentInterface;
+use Sylius\Component\Core\Model\PaymentInterface;
 use Payum\Core\Request\Capture;
-use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Webmozart\Assert\Assert;
 
 final class CaptureAction implements ActionInterface, ApiAwareInterface
 {
     /** @var SyliusApi */
     private $api;
-    /**
-     * @var RepositoryInterface
-     */
-    private $countryRepository;
-
-    public function __construct(RepositoryInterface $countryRepository)
-    {
-        $this->countryRepository = $countryRepository;
-    }
 
     public function execute($request): void
     {
         RequestNotSupportedException::assertSupports($this, $request);
-
-        /** @var SyliusPaymentInterface $payment */
+        /** @var PaymentInterface $payment */
         $payment = $request->getModel();
-        /** @var OrderInterface $orderData */
+        /** @var OrderInterface $order */
         $order = $request->getFirstModel()->getOrder();
-        $paymentData = $this->preparePaymentData($order, $payment);
+
+        throw new HttpPostRedirect(
+            SyliusApi::PAYMENT_URL,
+            $this->prepareOrder($order, $payment)
+        );
     }
 
-    private function preparePaymentData(OrderInterface $order, SyliusPaymentInterface $payment)
+    public function supports($request): bool
+    {
+        return
+            $request instanceof Capture &&
+            $request->getModel() instanceof PaymentInterface;
+    }
+
+    private function prepareOrder(OrderInterface $order, PaymentInterface $payment)
     {
         /** @var CustomerInterface $customer */
         $customer = $order->getCustomer();
@@ -78,25 +76,10 @@ final class CaptureAction implements ActionInterface, ApiAwareInterface
             )
         );
 
-        /** @var CountryInterface $country */
-        $country = $this->countryRepository->findOneBy(['code' => $billingAddress->getCountryCode()]);
-        Assert::notNull(
-            $country,
-            'Country with name %s does not exist'
-        );
-        Assert::isInstanceOf(
-            $country,
-            CountryInterface::class,
-            sprintf(
-                'Make sure the first model is the %s instance.',
-                CountryInterface::class
-            )
-        );
-
         $paymentData = [
             'fname' => (string) $billingAddress->getFirstName(),
             'lname' => (string) $billingAddress->getLastName(),
-            'country' => (string) $country->getName(),
+            'country' => (string) $billingAddress->getCountryCode(),
             'company' => (string) $billingAddress->getCompany(),
             'city' => (string) $billingAddress->getCity(),
             'state' => (string) $billingAddress->getProvinceName(),
@@ -133,7 +116,7 @@ final class CaptureAction implements ActionInterface, ApiAwareInterface
         $paymentData = array_merge($paymentData, [
             'sfname' => (string) $shippingAddress->getFirstName(),
             'slname' => (string) $shippingAddress->getLastName(),
-            'scountry' => (string) $country->getName(),
+            'scountry' => (string) $shippingAddress->getCountryCode(),
             'scompany' => (string) $shippingAddress->getCompany(),
             'scity' => (string) $shippingAddress->getCity(),
             'sstate' => (string) $shippingAddress->getProvinceName(),
@@ -147,14 +130,9 @@ final class CaptureAction implements ActionInterface, ApiAwareInterface
         return $paymentData;
     }
 
-    public function supports($request): bool
-    {
-        return
-            $request instanceof Capture &&
-            $request->getModel() instanceof SyliusPaymentInterface
-        ;
-    }
-
+    /**
+     * {@inheritdoc}
+     */
     public function setApi($api): void
     {
         if (!$api instanceof SyliusApi) {
